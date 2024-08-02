edd9175a9724c7fd4b37b9b5a68a0c067b70b451
BIKRAMSINGH1221 <107638046+BIKRAMSINGH1221@users.noreply.github.com>
2024-08-02 20:46:38 +0530

    New Commit

--- db.js
+++ db.js
@@ -2,15 +2,33 @@ import pg from "pg"
 import env from "dotenv";
 env.config();
 
-const db = new pg.Client({
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_POST,
-  });
+// const db = new pg.Client({
+//     user: process.env.DB_USER,
+//     host: process.env.DB_HOST,
+//     database: process.env.DATABASE,
+//     password: process.env.DB_PASSWORD,
+//     port: process.env.DB_POST,
+//   });
 
-db.connect();
+
+  const dbConfig = {
+    connectionString: process.env.DB_URL,
+    ssl: process.env.PG_SSL === 'true' ? { rejectUnauthorized: false } : false,
+    keepAlive: true,
+    schema : 'public',
+    connectionTimeoutMillis: 30000,  // 30 seconds timeout for connection
+    idle_in_transaction_session_timeout: 60000  // 60 seconds idle timeout
+  };
+
+  const db = new Client(dbConfig);
+
+  db.connect()
+  .then(() => console.log('Connected to the database'))
+  .catch(err => {
+     console.error('Database connection error:', err.stack);
+     process.exit(1);
+   });
+ 
   
 
 export default db;
