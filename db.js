2868163b82453c641e75b5c43c9422ac7d6d46b4
BIKRAMSINGH1221 <107638046+BIKRAMSINGH1221@users.noreply.github.com>
2024-08-02 21:09:54 +0530

    New db.js

--- db.js
+++ db.js
@@ -3,16 +3,17 @@ import env from "dotenv";
 const { Client } = pg;
 env.config();
 
-const db = new pg.Client({
-  
-   connectionString: process.env.DB_URL,
-    user: process.env.DB_USER,
-    host: process.env.DB_HOST,
-    database: process.env.DATABASE,
-    password: process.env.DB_PASSWORD,
-    port: process.env.DB_POST,
-    ssl : false
-  });
+const db = new Client({
+  connectionString: process.env.DB_URL,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DATABASE,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_POST,
+  ssl: {
+    rejectUnauthorized: false // Set to true if you want to reject self-signed certificates
+  }
+});
 
 db.connect();
   
